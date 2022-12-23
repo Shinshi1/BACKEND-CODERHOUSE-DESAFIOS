a49aaad113bd01d14d4deab996b4c7f8dbc4435a
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2022-12-23 15:36:43 -0300

    Se ordeno el directorio

---
+++ desafio1/index.js
@@ -0,0 +1,68 @@
+
+class ProductManager {
+    static id = 0;
+    constructor(title, description, price, thumbnail, stock) {
+        this.products = []
+        this.code = ProductManager.id++;
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.stock = stock
+    }
+    addProduct(product) {
+        let codeUsed = this.products.some(item => item.code === product.code)
+
+        // agregar un producto
+        if (product.title && product.description && product.price && product.thumbnail && product.code && product.stock && !codeUsed) {
+            this.products.push({
+                title: product.title,
+                description: product.description,
+                price: product.price,
+                thumbnail: product.thumbnail,
+                code: product.code,
+                stock: product.stock,
+            })
+            console.log(`Producto ${product.title} agregado`)
+            // console.log('Productos...', this.products)
+        } else {
+            console.error(`Error: Code repetido. El code ${product.code} ya esta en uso`)
+        }
+    }
+    getProducts() {
+        //retornar todos lo productos
+        return this.products
+    }
+    getProductById(id) {
+        // retornar el producto que cuente con este id
+        let productFound = this.products.find(prod => prod.code === id)
+        if (productFound) {
+            return productFound
+        } else {
+            console.error(`no product found with id ${id}`)
+        }
+    }
+}
+
+
+const gestionProd = new ProductManager()
+
+// Creación Productos
+const naranja = new ProductManager('naranja', 'agria y muy dulce', 140, 'https://cdn.pixabay.com/photo/2017/01/20/15/06/oranges-1995056_960_720.jpg', 23)
+const banana = new ProductManager('banana', 'muy dulce', 200, 'https://cdn.pixabay.com/photo/2017/06/27/22/21/banana-2449019_960_720.jpg', 30)
+const limon = new ProductManager('limón', 'muy agrio y jugoso', 190, 'https://cdn.pixabay.com/photo/2015/10/13/15/16/lemons-986304__340.jpg', 19)
+
+// TEST
+
+gestionProd.addProduct(naranja)
+gestionProd.addProduct(banana)
+gestionProd.addProduct(limon)
+// Repito el objeto para simular error de Code repetido
+console.log(gestionProd.addProduct(naranja))
+
+// Busco un producto con un id existente y uno inexistente
+console.log(gestionProd.getProductById(1))
+console.log(gestionProd.getProductById(100))
+
+// Hago uso de la funcion getProducts para ver cuantos productos tengo en el array products
+console.table(gestionProd.getProducts())
