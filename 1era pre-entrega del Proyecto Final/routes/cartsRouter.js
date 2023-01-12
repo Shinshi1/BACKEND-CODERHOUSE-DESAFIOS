153cf65412fea8039ff21212abacc9b3b18cac2f
gabriel hidalgo <gabi_hidalgo1@hotmail.com>
2023-01-11 21:34:44 -0300

    1er pre-entrega de Proyecto final esqueleto 0.1.0

--- 1era pre-entrega del Proyecto Final/routes/cartsRouter.js
+++ 1era pre-entrega del Proyecto Final/routes/cartsRouter.js
@@ -5,18 +5,30 @@ const cartsRouter = express.Router()
 // Methods
 cartsRouter.post('/', (req, res) => {
     // crear el carrito
-    res.send('carrito creado')
+    
+    res.status(200).send('cart created')
 });
 
 cartsRouter.get('/:cid', (req, res) => {
     const carritoId = req.params.cid
     // mostrar los productos que pertenezcan al carrito con ese cid
+    // const carts = ...
+    cart = carts.find((c) = c.id === Number(carritoId))
+    res.status(200).send(cart)
 });
 
 cartsRouter.post('/:cid/product/:pid', (req, res) => {
     const carritoId = req.params.cid
     const productoId = req.params.pid
     // agregar el producto al carrito
+    // const carts = ...
+    cart = carts.find((c) => c.id === Number(carritoId))
+    const product = {
+        code: Number(productoId),
+        quantity: 1
+    }
+    cart.push(Number(productoId))
+    res.status(200).send('product added to cart')
 });
 
 
--- 1era pre-entrega del Proyecto Final/routes/productsRouter.js
+++ 1era pre-entrega del Proyecto Final/routes/productsRouter.js
@@ -4,23 +4,68 @@ const productsRouter = express.Router()
 
 // Methods
 productsRouter.get('/', (req, res) => {
-    // deberá listar todos los productos de la base. (Incluyendo la limitación ?limit del desafío anterior
+    // Deberá listar todos los productos de la base. (Incluyendo la limitación ?limit del desafío anterior
+
+    //Debe recibir los productos ↓ 
+    // const products = ...
+    const limit = req.query.limit
+    let respuesta = products;
+
+    if (limit && !isNaN(Number(limit))) {
+        respuesta = products.slice(0, limit)
+    }
+    res.status(200).send(respuesta)
 });
 
 productsRouter.get('/:pid', (req, res) => {
     // deberá traer sólo el producto con el id proporcionado.
+    const { pid } = req.params;
+    // const products = ...
+    const product = products.find((p) => p.code === Number(pid));
+    res.status(200).send(product)
 });
 
 productsRouter.post('/', (req, res) => {
     // deberá agregar un nuevo producto con sus campos correspondientes.
+    // const products = ...
+    const product = {
+        title: req.body.title,
+        description: req.body.description,
+        code: Number(),
+        price: Number(req.body.price),
+        status: true,
+        stock: Number(req.body.stock),
+        category: req.body.category,
+        thumbnail: [req.body.thumbnail],
+    }
+    res.status(200).send('product added')
 });
 
 productsRouter.put('/:pid', (req, res) => {
     // deberá tomar un producto y actualizarlo por los campos enviados desde body. NUNCA se debe actualizar o eliminar el id al momento de hacer dicha actualización.
+    const { pid } = req.params
+    // const products = ...
+    productToUpdate = products.find((p) => p.code === Number(pid))
+    productUpdated = {
+        ...productToUpdate,
+        title: req.body.title,
+        code: Number(pid), // productToUpdated.code
+        price: Number(req.body.price),
+        status: true,
+        stock: Number(req.body.stock),
+        category: req.body.category,
+        thumbnail: [req.body.thumbnail] //opcional
+    }
+    res.status(200).send('product updated')
 })
 
 productsRouter.delete('/:pid', (req, res) => {
     // deberá eliminar el producto con el pid indicado.
+    const { pid } = req.params
+    // const products = ...
+    products = prodcuts.filter((p) => p.code !== Number(pid));
+    res.status(200).send('product deleted')
+    
 })
 
 // exports
